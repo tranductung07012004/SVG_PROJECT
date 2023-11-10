cbc0756dd1611daae92b4e461a2eb2a315509126
tdt-universityofscience <122892121+tranductung07012004@users.noreply.github.com>
2023-11-10 08:07:35 +0700

    Update SVGDemo.cpp

--- SVGDemo.cpp
+++ SVGDemo.cpp
@@ -324,3 +324,333 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
       return DefWindowProc(hWnd, message, wParam, lParam);
    }
 } // WndProc
+
+//////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+// Ở đây Ở đây
+HWND g_hWnd;
+HINSTANCE g_hInstance;
+Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+ULONG_PTR gdiplusToken;
+bool isButtonClicked = false;
+bool isScrollBarVisible = false;
+HWND g_hScrollBar = NULL;
+int g_nScrollPos = 0;
+int first_time = 1;
+
+VOID OnPaint(HDC hdc, float zoomFactor)
+{
+   Graphics graphics(hdc);
+   const string filename = "sample.svg";
+   vector<SVGElement> elements = parseSVG(filename);
+
+   // Print the elements
+   Matrix zoomTransform(zoomFactor, 0.0f, 0.0f, zoomFactor, 0.0f, 0.0f);
+   graphics.SetTransform(&zoomTransform);
+
+   for (const SVGElement& element : elements)
+   {
+       if (element.type == "rect") {
+           RectSVG rectElement;
+           rectElement.parseShapeSVG(element);
+           rectElement.drawRectSVG(graphics);
+       }
+       if (element.type == "text") {
+           TextSVG textElement;
+           textElement.parseShapeSVG(element);
+           textElement.drawTextSVG(graphics);
+       }
+       if (element.type == "circle") {
+           CircleSVG circleElement;
+           circleElement.parseShapeSVG(element);
+           circleElement.drawCircleSVG(graphics);
+       }
+       if (element.type == "polyline") {
+           PolylineSVG polylineElement;
+           polylineElement.parseShapeSVG(element);
+           polylineElement.drawPolylineSVG(graphics);
+       }
+       if (element.type == "ellipse") {
+           EllipseSVG ellipseElement;
+           ellipseElement.parseShapeSVG(element);
+           ellipseElement.drawEllipseSVG(graphics);
+       }
+       if (element.type == "line") {
+           LineSVG lineElement;
+           lineElement.parseShapeSVG(element);
+           lineElement.drawLineSVG(graphics);
+       }
+       if (element.type == "polygon") {
+           PolygonSVG polygonElement;
+           polygonElement.parseShapeSVG(element);
+           polygonElement.drawPolygonSVG(graphics);
+       }
+   }
+
+}
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
+{
+   HWND                hWnd;
+   MSG                 msg;
+   WNDCLASS            wndClass;
+   GdiplusStartupInput gdiplusStartupInput;
+   ULONG_PTR           gdiplusToken;
+
+   // Read XML
+  // xml_document<> doc;
+  // xml_node<> *rootNode;
+   // Read the xml file into a vector
+  /* ifstream file("sample.svg");
+   vector<char> buffer((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+   buffer.push_back('\0');
+   // Parse the buffer using the xml file parsing library into doc 
+   doc.parse<0>(&buffer[0]);
+
+   rootNode = doc.first_node();
+   xml_node<> *node = rootNode->first_node();
+
+   while (node != NULL) {
+	   char *nodeName = node->name();
+	   xml_attribute<> *firstAttribute = node->first_attribute();
+	   char *attributeName = firstAttribute->name();
+	   char *attributeValue = firstAttribute->value();
+	   xml_attribute<> *secondAttribute = firstAttribute->next_attribute();
+	   // Set breakpoint here to view value
+	   // Ref: http://rapidxml.sourceforge.net/manual.html
+	   node = node->next_sibling();
+   }*/
+   
+   
+
+   
+   // Initialize GDI+.
+   GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+   
+   wndClass.style          = CS_HREDRAW | CS_VREDRAW;
+   wndClass.lpfnWndProc    = WndProc;
+   wndClass.cbClsExtra     = 0;
+   wndClass.cbWndExtra     = 0;
+   wndClass.hInstance      = hInstance;
+   wndClass.hIcon          = LoadIcon(NULL, IDI_APPLICATION);
+   wndClass.hCursor        = LoadCursor(NULL, IDC_ARROW);
+   wndClass.hbrBackground  = (HBRUSH)GetStockObject(WHITE_BRUSH);
+   wndClass.lpszMenuName   = NULL;
+   wndClass.lpszClassName  = TEXT("GettingStarted");
+   
+   RegisterClass(&wndClass);
+   
+   hWnd = CreateWindow(
+      TEXT("GettingStarted"),   // window class name
+      TEXT("SVG Demo"),  // window caption
+      WS_OVERLAPPEDWINDOW,      // window style
+      CW_USEDEFAULT,            // initial x position
+      CW_USEDEFAULT,            // initial y position
+      CW_USEDEFAULT,            // initial x size
+      CW_USEDEFAULT,            // initial y size
+      NULL,                     // parent window handle
+      NULL,                     // window menu handle
+      hInstance,                // program instance handle
+      NULL);                    // creation parameters
+
+   ShowWindow(hWnd, iCmdShow);
+   UpdateWindow(hWnd);
+   
+   while(GetMessage(&msg, NULL, 0, 0))
+   {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+   }
+   
+   GdiplusShutdown(gdiplusToken);
+   return msg.wParam;
+}  // WinMain
+
+void HandleButtonClick(HWND hWnd, POINT point)
+{
+    RECT buttonRect;
+    GetClientRect(hWnd, &buttonRect);
+    buttonRect.left += 400;
+    buttonRect.top += 470;
+    buttonRect.right = 500;
+    buttonRect.bottom = 500;
+
+    // Check if the button is clicked
+    if (PtInRect(&buttonRect, point))
+    {
+        // Toggle the button state
+        isButtonClicked = !isButtonClicked;
+
+        // Toggle the scrollbar visibility
+
+         isScrollBarVisible = !isScrollBarVisible;
+
+        // Reset the scrollbar position
+        //g_nScrollPos = 0;
+
+        // Show or hide the scrollbar
+        ShowWindow(g_hScrollBar, isScrollBarVisible ? SW_SHOWNORMAL : SW_HIDE);
+
+        // Invalidate the entire window to redraw both button and scrollbar
+        InvalidateRect(hWnd, NULL, TRUE);
+    }
+}
+
+void DrawButton(HDC hdc)
+{
+    RECT rect;
+    rect.left = 400;    // Specify the left coordinate of the button
+    rect.top = 470;     // Specify the top coordinate of the button
+    rect.right = 500;   // Specify the right coordinate of the button
+    rect.bottom = 500;  // Specify the bottom coordinate of the button
+
+    // Draw the button
+    if (isButtonClicked)
+    {
+        // Button is clicked, draw it in a different color
+        FillRect(hdc, &rect, (HBRUSH)GetStockObject(GRAY_BRUSH));
+    }
+    else
+    {
+        // Button is not clicked, draw it in a different color
+        FillRect(hdc, &rect, (HBRUSH)GetStockObject(DKGRAY_BRUSH));
+    }
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    static float zoomFactor = 1.0f;
+    static POINT lastMousePos;
+
+    static HDC hdcBuffer = nullptr;
+    static HBITMAP hBitmap = nullptr;
+
+
+    switch (message) {
+    case WM_CREATE:
+    {
+        // Create the scrollbar without showing it initially
+        g_hScrollBar = CreateWindowEx(0, L"SCROLLBAR", NULL, WS_CHILD | SBS_HORZ, 400, 440, 100, 20, hWnd, NULL, g_hInstance, NULL);
+
+        // Set the scrollbar range and position
+        SetScrollRange(g_hScrollBar, SB_CTL, 0, 100, TRUE);
+        SetScrollPos(g_hScrollBar, SB_CTL, g_nScrollPos, TRUE);
+
+        break;
+    }
+
+    case WM_HSCROLL:
+    {
+        int nScrollCode = LOWORD(wParam);  // Scroll code
+        int nPos = HIWORD(wParam);         // New scroll position
+
+        switch (nScrollCode)
+        {
+        case SB_LINELEFT:
+            g_nScrollPos -= 5;
+            break;
+        case SB_LINERIGHT:
+            g_nScrollPos += 5;
+            break;
+        //case SB_THUMBPOSITION:
+        case SB_THUMBTRACK:
+            g_nScrollPos = nPos;
+            break;
+        }
+
+        // Update the scroll position of the scrollbar control
+        SetScrollPos(g_hScrollBar, SB_CTL, g_nScrollPos, TRUE);
+
+        // Redraw the window
+        //InvalidateRect(hWnd, NULL, TRUE);
+        UpdateWindow(hWnd);
+
+        break;
+    }
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        RECT clientRect;
+        GetClientRect(hWnd, &clientRect);
+        int clientWidth = clientRect.right - clientRect.left;
+        int clientHeight = clientRect.bottom - clientRect.top;
+
+        int rectWidth = static_cast<int>(50 * zoomFactor);
+        int rectHeight = static_cast<int>(50 * zoomFactor);
+        int rectX = (clientWidth - rectWidth) / 2;
+        int rectY = (clientHeight - rectHeight) / 2;
+
+        if (!hdcBuffer) {
+            hdcBuffer = CreateCompatibleDC(hdc);
+            hBitmap = CreateCompatibleBitmap(hdc, clientWidth, clientHeight);
+            SelectObject(hdcBuffer, hBitmap);
+        }
+
+        Graphics graphicsBuffer(hdcBuffer);
+        graphicsBuffer.Clear(Color(255, 255, 255, 255)); // Set the background to white
+        OnPaint(hdcBuffer, zoomFactor);
+        DrawButton(hdcBuffer);
+
+        // Copy the buffer to the screen
+        BitBlt(hdc, 0, 0, clientWidth, clientHeight, hdcBuffer, 0, 0, SRCCOPY);
+
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    
+    case WM_MOUSEWHEEL:
+    {
+        int delta = GET_WHEEL_DELTA_WPARAM(wParam);
+
+        if (delta > 0)
+            zoomFactor *= 1.01f;  // Increase the zoom factor by 10%
+        else
+            zoomFactor /= 1.01f;  // Decrease the zoom factor by 10%
+
+        // Update the scrollbar position based on the zoom factor
+        g_nScrollPos = static_cast<int>(g_nScrollPos * zoomFactor);
+
+        // Clear the buffer for the next paint
+        if (hdcBuffer) {
+            Graphics graphicsBuffer(hdcBuffer);
+            graphicsBuffer.Clear(Color(255, 255, 255, 255));  // Set the background to white
+        }
+
+        // Update the scrollbar range based on the new zoomed content size
+        int newMaxPos = static_cast<int>((100 * zoomFactor) - 100);
+        SetScrollRange(g_hScrollBar, SB_CTL, 0, newMaxPos, TRUE);
+
+        // Redraw the window
+        InvalidateRect(hWnd, NULL, TRUE);
+        UpdateWindow(hWnd);
+
+        break;
+    }
+    case WM_LBUTTONDOWN:
+        {
+            POINT pt;
+            pt.x = LOWORD(lParam);
+            pt.y = HIWORD(lParam);
+            HandleButtonClick(hWnd, pt);
+            break;
+        }
+
+    case WM_DESTROY:
+    {
+        if (hdcBuffer) {
+            DeleteDC(hdcBuffer);
+            DeleteObject(hBitmap);
+        }
+        PostQuitMessage(0);
+        break;
+    }
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
