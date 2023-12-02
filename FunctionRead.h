c79c44a2a2e6535085de50d0542b8e343c5db5d5
namphuonghoang <131260726+namphuonghoang@users.noreply.github.com>
2023-12-02 18:04:08 +0700

    Update FunctionRead.h

---
+++ FunctionRead.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "stdafx.h"
+#include "rapidxml.hpp"
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <regex>
+#include "SVGDemo.h"
+#include <windows.h>
+#include <objidl.h>
+#include <gdiplus.h>
+#include <locale>
+#include <codecvt>
+#include <unordered_map>.
+#include <cmath>
+using namespace std;
+using namespace rapidxml;
+using namespace Gdiplus;
+#pragma comment (lib,"Gdiplus.lib")
+
+
+struct SVGElement {
+    string type;
+    map<string, string> attributes;
+    string textContent;
+    vector<SVGElement> children;
+};
+
+vector<SVGElement> parseSVG(const string& filename, double& width, double& height);
+void parseStyle(const string& s, SVGElement& element);
+void parseSVGNode(xml_node<>* node, vector<SVGElement>& elements);
+void parseGroupNode(xml_node<>* node, SVGElement& groupElement);
